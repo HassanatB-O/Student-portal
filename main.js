aec7584706edf29c5f6df47433d5b772c69ef34f
HassanatB-O <60980025+HassanatB-O@users.noreply.github.com>
2020-04-23 14:59:09 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function toggleMenu()
+{
+    var menuBox = document.getElementById('menu-box');
+    if (menuBox.style.display == "block")
+    {
+        menuBox.style.display = "none";
+    } 
+    else 
+    {
+        menuBox.style.display = "block"
+    }
+}
